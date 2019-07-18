cfa4f32673f8f700b0da8e8b8e07f38a20be2cba
Dana Engebretson <dana.engebretson@gmail.com>
2019-07-18 16:23:44 -0700

    takes in an integer now - not a list - handles negative numbers with a zero at the end

--- reverse.py
+++ reverse.py
@@ -1,15 +1,27 @@
 class Solution:
 
     def reverse(self, x: int):
+        if self.is_thirty_two_bit(x) == False:
+            return 0
+        else:
+            x = list(str(x))
 
-        reversed_x = []
-        for num in x:
-            if self.is_thirty_two_bit(num) == False:
-                return 0
+            if x[0] == '-':
+                Negative = True
+                x = x[1:]
             else:
-                reversed_x = [num] + reversed_x
+                Negative = False
 
-        return reversed_x
+            reversed_x = ''
+
+            for num in x:
+                reversed_x = str(num) + reversed_x
+            reversed_x = int(reversed_x)
+
+            if Negative:
+                reversed_x = reversed_x*(-1)
+
+            return reversed_x
 
     def is_thirty_two_bit(self, x: int):
         if abs(x) <= 0xffffffff:
--- test_reverse.py
+++ test_reverse.py
@@ -4,29 +4,34 @@ import reverse
 class TestReverseMethod(unittest.TestCase):
 
     def test_reverse(self):
-        x = [1, 2, 3]
+        x = 123
         result = reverse.Solution().reverse(x)
-        expected_result = [3, 2, 1]
+        expected_result = 321
         self.assertEqual(result, expected_result)
 
     def test_not_thirty_two_bit(self):
-        x = [2**32, 2, 3]
+        x = 2**32
         result = reverse.Solution().reverse(x)
         expected_result = 0
         self.assertEqual(result, expected_result)
 
     def test_negative_not_thirty_two_bit(self):
-        x = [-2**32, 2, 3]
+        x = -2**32
         result = reverse.Solution().reverse(x)
         expected_result = 0
         self.assertEqual(result, expected_result)
-"""
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
-"""
+
+    def test_negative(self):
+        x = -123
+        result = reverse.Solution().reverse(x)
+        expected_result = -321
+        self.assertEqual(result, expected_result)
+
+    def test_negative_with_zero(self):
+        x = -120
+        result = reverse.Solution().reverse(x)
+        expected_result = -21
+        self.assertEqual(result, expected_result)
+
 if __name__ == '__main__':
     unittest.main()
